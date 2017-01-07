ec86b4720e649ada2356160a14db47a61b8d99ea
vagrant <vagrant@homestead>
2017-01-07 09:29:55 +0000

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, resp, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    resp.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('build'));
 
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
-  if (process.send) {
-    process.send('online');
+
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
+  // if (process.send) {
+  //   process.send('online');
     // process.send({
     //   event:'online',
     //   url:'http://react-weather.loc:3000/'
     // });
-  }
-})
+  // }
+});
