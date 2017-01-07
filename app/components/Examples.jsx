4938a7f3092dc9ceb2e533292ad697a217b96f25
vagrant <vagrant@homestead>
2017-01-07 09:53:45 +0000

    rename example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -12,7 +12,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component asdfasdf</h3>
+      <h3>Examples</h3>
       <p>some p </p>
     </div>
   )
