c101a09ad33d731329546b2ed2322ff7ff00e409
vagrant <vagrant@homestead>
2017-01-14 12:44:51 +0000

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,17 +18,54 @@ var {Link, IndexLink} = require('react-router');
 
 
 
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>Nav Compone nsss R</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-
-      <a href="#/about">got ot about</a>
-    </div>
-  )
-}
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    console.log('%c Error', 'background-color:yellow', 'Not yet wired up');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Nav;
+
+// var old = {
+//   <div>
+//     <h2>Nav Compone nsss R</h2>
+//     <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+//     <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+//     <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+//
+//     <a href="#/about">got ot about</a>
+//   </div>
+// }
